6d63c4fc2f049acadc9853a4cecc217c7ccf94d6
VadimL11 <dovbnia1994@gmail.com>
2026-01-26 18:39:15 +0100

    add ping controller

---
+++ src/main/java/org/example/loyaltycard/LoyaltycardApplication.java
@@ -0,0 +1,13 @@
+package org.example.loyaltycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltycardApplication {
+
+    public static void main() {
+        SpringApplication.run(LoyaltycardApplication.class);
+    }
+
+}
